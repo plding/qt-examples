d9c10ae1caa607f40de2cc82a5dbcaa5c0b7fbcc
plding <77676182@qq.com>
2018-06-14 19:46:41 +0800

    update

---
+++ itemviews/combowidgetmapper/main.cpp
@@ -0,0 +1,10 @@
+#include <QtGui>
+#include "window.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    Window window;
+    window.show();
+    return app.exec();
+}
---
+++ itemviews/combowidgetmapper/window.h
@@ -0,0 +1,44 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+class QComboBox;
+class QDataWidgetMapper;
+class QLabel;
+class QLineEdit;
+class QPushButton;
+class QStandardItemModel;
+class QStringListModel;
+class QTextEdit;
+QT_END_NAMESPACE
+
+class Window : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Window(QWidget *parent = 0);
+
+private slots:
+    void updateButtons(int row);
+
+private:
+    void setupModel();
+
+    QLabel *nameLabel;
+    QLabel *addressLabel;
+    QLabel *typeLabel;
+    QLineEdit *nameEdit;
+    QTextEdit *addressEdit;
+    QComboBox *typeComboBox;
+    QPushButton *nextButton;
+    QPushButton *previousButton;
+
+    QStandardItemModel *model;
+    QStringListModel *typeModel;
+    QDataWidgetMapper *mapper;
+};
+
+#endif  /* WINDOW_H */
