8d9bfd73e61597c87b6c4699b63b7a3894bf79ff
plding <77676182@qq.com>
2018-06-15 18:02:16 +0800

    update

---
+++ sql/tablemodel/tablemodel.cpp
@@ -0,0 +1,44 @@
+#include <QtGui>
+#include <QtSql>
+
+#include "../connection.h"
+
+void initializeModel(QSqlTableModel *model)
+{
+    model->setTable("person");
+    model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    model->select();
+
+    model->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
+    model->setHeaderData(1, Qt::Horizontal, QObject::tr("First name"));
+    model->setHeaderData(2, Qt::Horizontal, QObject::tr("Last name"));
+}
+
+QTableView *createView(QSqlTableModel *model, const QString &title = "")
+{
+    QTableView *view = new QTableView;
+    view->setModel(model);
+    view->setWindowTitle(title);
+
+    return view;
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    if (!createConnection())
+        return 1;
+
+    QSqlTableModel model;
+
+    initializeModel(&model);
+
+    QTableView *view1 = createView(&model, QObject::tr("Table Model (View 1)"));
+    QTableView *view2 = createView(&model, QObject::tr("Table Model (View 2)"));
+
+    view1->show();
+    view2->move(view1->x() + view1->width() + 20, view1->y());
+    view2->show();
+
+    return app.exec();
+}
